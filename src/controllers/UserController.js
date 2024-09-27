b5a615f14f5a89f3fa9aa0fd044e1ca5ad4572c8
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-09-27 09:14:20 +0530

    update delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -21,4 +21,27 @@ const getAllUsers = async (req, res) => {
 }
 
 
-export { addUser, getAllUsers }
+const deleteUser = async (req, res) => {
+    try {
+        let userid = req.body.userid
+        const deletedUser = await User.findByIdAndDelete(userid)
+        res.status(200).json(deletedUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        let uodatedUser = await User.findByIdAndUpdate({ _id: req.body.userid },
+            {
+                uemail: req.body.uemail
+            }, { new: false })
+
+        res.status(200).json(uodatedUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+export { addUser, getAllUsers, deleteUser, updateUser }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { addUser, getAllUsers } from "../controllers/UserController.js"
+import { addUser, deleteUser, getAllUsers, updateUser } from "../controllers/UserController.js"
 
 
 const router = express.Router()
 
 router.post("/adduser", addUser)
 router.get("/allusers", getAllUsers)
+router.delete("/deleteuser", deleteUser)
+router.put("/updateuser", updateUser)
 
 
 
